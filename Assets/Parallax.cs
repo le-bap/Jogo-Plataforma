096af06f73f0c5cb4cf30ea3e3deb0b62760a12a
LETIZIA LOWATZKI BAPTISTELLA <uniflbaptistella@feinet.fei.edu.br>
2026-04-08 22:01:15 -0300

    corrigindo parallax

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -2,37 +2,19 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float lenght;
+    private float length;
     public float parallaxEffect;
-
-    public KeyCode rightKey = KeyCode.D;
-    public KeyCode leftKey = KeyCode.A;
 
     void Start()
     {
-        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    void Update()
-    {
-        float direction = 0;
-
-        if (Input.GetKey(rightKey))
-            direction = -1; // fundo vai pro lado contrário
-
-        if (Input.GetKey(leftKey))
-            direction = 1;
-
-        transform.position += Vector3.right * direction * Time.deltaTime * parallaxEffect;
-
-        // Loop do background
-        if (transform.position.x < -lenght)
-        {
-            transform.position = new Vector3(lenght, transform.position.y, transform.position.z);
-        }
-        else if (transform.position.x > lenght)
-        {
-            transform.position = new Vector3(-lenght, transform.position.y, transform.position.z);
+    void Update(){
+        transform.position += Vector3.left * Time.deltaTime * parallaxEffect;
+        if (transform.position.x < -length) {
+            transform.position = new Vector3(length, transform.position.y, transform.position.z);
         }
     }
+
 }
